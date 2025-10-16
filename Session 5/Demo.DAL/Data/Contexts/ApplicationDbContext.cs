30bfd05e8cc25e1b67b5c7631dfd93d52ebdb4af
higzo <ashrafhgazy9@gmail.com>
2025-10-16 21:03:57 +0300

    initial

---
+++ Session 5/Demo.DAL/Data/Contexts/ApplicationDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Demo.DAL.Data.Configurations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo.DAL.Data.Contexts
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(): base()
+        {
+
+        }
+        public DbSet<Department> Departments {get; set;} 
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("");
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration<Department>(new DepartmentCofigurations());
+        }
+    }
+}
